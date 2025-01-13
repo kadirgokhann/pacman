b270cef660b4696a414a48ad878af9012bf663b1
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2025-01-13 23:03:56 +0300

    Update PacManGame.java

--- PacManGame.java
+++ PacManGame.java
@@ -2,12 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class PacManGame extends JPanel implements KeyListener {
 
     private static final    int TILE_SIZE   = 15; 
-    private                 int pacmanX     = 1; 
-    private                 int pacmanY     = 1;
+    private                 int pacmanX = 1;
+    private int pacmanY = 1;
+    private int[][]    ghosts = {{13, 13}, {13, 14}, {14, 13}, {14, 14}};
+    private                 boolean[][] dots;
+    private                 int score = 0;
 
     private int[][] map = {  //26
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -22,9 +26,9 @@ public class PacManGame extends JPanel implements KeyListener {
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},       
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
@@ -51,25 +55,71 @@ public class PacManGame extends JPanel implements KeyListener {
         frame.add(this);
         frame.addKeyListener(this);
         frame.setVisible(true);
+        dots = new boolean[map.length][map[0].length];
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                if (map[y][x] == 0) {
+                    dots[y][x] = true;
+                }
+            }
+        }
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
                 if (map[y][x] == 0) {
-                    g.setColor(Color.BLUE);
-                    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-                } else {
                     g.setColor(Color.BLACK);
                     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                } else if (map[y][x] == 1) {
+                    g.setColor(Color.BLUE);
+                    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                } else if (map[y][x] == 2) {
+                    g.setColor(Color.GRAY);
+                    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+                if (dots[y][x]) {
+                    g.setColor(Color.WHITE);
+                    g.fillOval(x * TILE_SIZE + TILE_SIZE / 4, y * TILE_SIZE + TILE_SIZE / 4, TILE_SIZE / 2, TILE_SIZE / 2);
                 }
             }
         }
         
         g.setColor(Color.YELLOW);
         g.fillOval(pacmanX * TILE_SIZE, pacmanY * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        if (dots[pacmanY][pacmanX]) {
+            score++;
+            dots[pacmanY][pacmanX] = false;
+        }
+        g.setColor(Color.WHITE);
+        g.drawString("Puan: " + score, 10, 10);
+
+        for (int i = 0; i < ghosts.length; i++) {
+            if (ghosts[i][0] < pacmanX && map[ghosts[i][1]][ghosts[i][0] + 1] != 1 ) {
+                ghosts[i][0]++;
+            } else if (ghosts[i][0] > pacmanX && map[ghosts[i][1]][ghosts[i][0] - 1] != 1) {
+                ghosts[i][0]--;
+            } else if (ghosts[i][1] < pacmanY && map[ghosts[i][1] + 1][ghosts[i][0]] != 1) {
+                ghosts[i][1]++;
+            } else if (ghosts[i][1] > pacmanY && map[ghosts[i][1] - 1][ghosts[i][0]] != 1) {
+                ghosts[i][1]--;
+            }
+        }
+
+        ArrayList<Color> colors = new ArrayList<>();
+        colors.add(Color.RED);
+        colors.add(Color.CYAN);
+        colors.add(Color.PINK);
+        colors.add(Color.ORANGE);
+
+        for (int i = 0; i < ghosts.length; i++) {
+            g.setColor(colors.get(i));
+            g.fillOval(ghosts[i][0] * TILE_SIZE, ghosts[i][1] * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+
     }
 
     @Override
@@ -84,20 +134,20 @@ public class PacManGame extends JPanel implements KeyListener {
         } else if (key == KeyEvent.VK_RIGHT && map[pacmanY][pacmanX + 1] != 1) {
             pacmanX++;
         }
-        repaint(); // Ekranı yenile
+        repaint(); 
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // Kullanılmıyor
+    
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        // Kullanılmıyor
+    
     }
 
     public static void main(String[] args) {
-        new PacManGame(); // Oyunu başlat
+        new PacManGame(); 
     }
 }
